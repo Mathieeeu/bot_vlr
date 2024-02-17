5631fd1007a11ec81c7e36cf42f0bfcfe6629dd6
Mathieu <77482141+Mathieeeu@users.noreply.github.com>
2024-02-17 14:01:05 +0100

    setting up the bot discord

--- bot_vlr.py
+++ bot_vlr.py
@@ -1,1 +1,14 @@
+import os
 import discord
+from discord.ext import commands
+import time
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = commands.Bot(command_prefix='!',intents=intents)
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Game(name="Valorant"))
+    print("ok")
